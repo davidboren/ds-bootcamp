d2ebbe6fb70484d16b8a4307b1ab653c01e965fb
davidboren <daveb@fair.com>
2019-05-31 13:50:59 -0700

    Add models

--- ds_bootcamp/flask_app.py
+++ ds_bootcamp/flask_app.py
@@ -1,16 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+import yaml
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def create_app():
     app = Flask(__name__)
+    
+    with open(os.path.join(dir_path, 'yamls', 'dave_model.yaml')) as f:
+        dave_model = yaml.load(f, Loader=yaml.SafeLoader)
 
-    @app.route("/")
-    def hello_world():
+    @app.route("/dave")
+    def dave_hello_world():
         return "Hello, World!", 200
 
-    @app.route("/bye")
-    def goodby_world():
-        raise Exception("BLAHD")
-        return "Goodbye, World!", 200
+    @app.route("/dave_model/<submodel>/<x_1>")
+    def dave_yaml_model(submodel, x_1):
+        try:
+            model = dave_model[submodel]
+        except KeyError:
+            return 'Model not found', 404
+        dict_ = {'score': model['b_0'] + model['b_1'] * int(x_1)}
+        return jsonify(dict_)
 
     return app
--- tests/test_daveb.py
+++ tests/test_daveb.py
@@ -1,8 +1,12 @@
-
-def test_get_meta(client):
-    res = client.get("/")
+def test_hello(client):
+    res = client.get("/dave")
     assert res.status_code == 200
 
-def test_bye(client):
-    res = client.get("/bye")
+def test_dave_model(client):
+    res = client.get("/dave_model/submodel_1/10")
     assert res.status_code == 200
+    assert res.json['score'] == 10
+
+def test_dave_model_404(client):
+    res = client.get("/dave_model/submodel_3/10")
+    assert res.status_code == 404
